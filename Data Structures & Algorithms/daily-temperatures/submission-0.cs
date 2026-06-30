4db14ba8b189e412aa15c17b2ee088dc3bb11bda
KienVan <ngokienvan2005@gmail.com>
2026-06-30 08:43:25 +0700

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] DailyTemperatures(int[] temperatures) {
+        Stack<int[]> stack = new Stack<int[]>();
+        int[] res = new int[temperatures.Length];
+
+        for(int i = 0; i < temperatures.Length; i++){
+            int t = temperatures[i];
+            while(stack.Count > 0 && t > stack.Peek()[1]){
+                int[] pair = stack.Pop();
+                res[pair[0]] = i - pair[0];
+            }
+            stack.Push(new int[] {i, t});
+        }
+        return res;
+    }
+}
