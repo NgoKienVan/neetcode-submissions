b6c78f1229ced459b1ea7a7652bacfd7a05103b4
KienVan <ngokienvan2005@gmail.com>
2026-07-04 17:13:58 +0700

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int MaxArea(int[] heights) {
+        int l = 0;
+        int r = heights.Length - 1;
+        int max = 0;
+
+        while(l < r){
+            int area = Math.Min(heights[l], heights[r]) * (r - l);
+            max = Math.Max(max, area);
+
+            if(heights[l] <= heights[r]){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        return max;
+    }
+}
