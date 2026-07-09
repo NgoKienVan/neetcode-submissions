f48d3654e32fbcc8fc87aa9f81d9749bfe6872ad
KienVan <ngokienvan2005@gmail.com>
2026-07-10 05:30:19 +0700

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        char[] s1 = s.ToCharArray();
+        char[] t1 = t.ToCharArray();
+
+        Array.Sort(s1);
+        Array.Sort(t1);
+
+        string s2 = new string(s1);
+        string t2 = new string(t1);
+        return s2 == t2;
+    }
+}
